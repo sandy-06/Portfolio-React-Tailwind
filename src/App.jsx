7f5f8e39d2374422d71d2c160cdee9fdc37e7f27
Sandra Sanders <sandy-sanders@msn.com>
2024-09-07 11:11:42 -0700

    first update on skills and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
 import Service from './components/Service'
+import Skills from './components/Skills'
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
         <Hero />
         <About />
         <Service />
+        <Skills />
         <Projects />
         <Contact />
         <Footer />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-black text-white px-8 md:px-16 lg:px-24'>
+    <nav className='bg-black text-white px-8 md:px-16 lg:px-24 sticky'>
         <div className='container py-2 flex justify-center md:justify-between items-center'>
-            <div className='text-2xl font-bold hidden md:inline'>Yousof</div>
+            <div className='text-2xl font-bold hidden md:inline'>Sanders</div>
             <div className='space-x-6'>
                 <a href="#home" className='hover:text-gray-400'>Home</a>
                 <a href="#about" className='hover:text-gray-400'>About Me</a>
                 <a href="#service" className='hover:text-gray-400'>Services</a>
+                <a href="#skill" className='hover:text-gray-400'>Skills</a>
                 <a href="#project" className='hover:text-gray-400'>Projects</a>
                 <a href="#contact" className='hover:text-gray-400'>Contact</a>
             </div>
